dc6cf7e4c7baa587306cda746852fac4570f00cd
Evgenyi <VRN.Ocenka@gmail.com>
2025-01-07 20:28:23 +0300

    Added Localization

---
+++ Assets/Scripts/LocalzationSelector.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+public class LocalzationSelector : MonoBehaviour
+{
+    private bool _active;
+    public LocalizedString localizedString;
+
+    private void Start()
+    {
+        int ID = PlayerPrefs.GetInt("LocaleKey", 0);
+        ChangeLocale(ID);
+        localizedString.StringChanged += OnStringChanged;
+
+        // Получение значения сразу
+        string value = localizedString.GetLocalizedString();
+        Debug.Log("Полученное значение: " + value);
+    }
+
+    public void ChangeLocale(int localeID)
+    {
+        if (_active) return;
+        StartCoroutine(SetLocale(localeID));
+    }
+
+    IEnumerator SetLocale(int localeID)
+    {
+        _active = true;
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[localeID];
+        _active = false;
+        PlayerPrefs.SetInt("LocaleKey", localeID);
+    }
+    private void OnStringChanged(string newValue)
+    {
+        Debug.Log("Измененная строка: " + newValue);
+    }
+    
+}
