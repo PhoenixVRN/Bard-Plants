bb65be82c441507a7fef74142d7ea7cda6738baa
Evgenyi <VRN.Ocenka@gmail.com>
2025-03-18 15:58:49 +0300

    build 16

--- Assets/Scripts/Reference.cs
+++ Assets/Scripts/Reference.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public static class Reference
 {
-    // internal static Camera MainCamera => _mainCamera != null ? _mainCamera : _mainCamera = GameObject.FindObjectOfType<TagMainCamera>().GetComponent<Camera>();
-    // private static Camera _mainCamera;
-    //
     internal static GameModel GameModel => _gameModel != null ? _gameModel : _gameModel = new GameModel();
     private static GameModel _gameModel;
     internal static List<Fetus> AllFetus = new List<Fetus>();
